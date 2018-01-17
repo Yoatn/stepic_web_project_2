7276464da4e72b2a4bc74b0fd73ef75dc4b828a5
Yoatn <31929312+Yoatn@users.noreply.github.com>
2018-01-18 00:29:16 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,4 @@
-
-sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-sudo rm -r /etc/gunicorn.d/*
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
