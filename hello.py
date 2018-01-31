644f45697adcbc6e0877deec297254b7b2d9e4cc
Yoatn <31929312+Yoatn@users.noreply.github.com>
2018-01-31 19:14:19 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def app(envirov, start_response):
+def application(envirov, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [bytes('\r\n'.join(envirov['QUERY_STRING'].split('&')),
             encoding='utf8')]
